148b262a0fba3398fabf8b48deca3577e3b4ee89
Riza <rizakurniawan48@gmail.com>
2021-06-05 19:29:03 +0700

    test

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --bind 0.0.0.0:5000 app:app --max-requests 2 
+web: gunicorn
--- app.py
+++ app.py
@@ -17,4 +17,4 @@ def home():
     return 'Hello World'
 
 if __name__=='__main__':
-    app.run(port = 5000, debug = True)
+    app.run()
