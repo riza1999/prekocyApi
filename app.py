9d06ce170be1d83d56f34db6615d9fa02d2c5d34
Riza <rizakurniawan48@gmail.com>
2021-06-05 21:51:30 +0700

    edit

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1eKactgUbnbYdhlRJZqzPu-egBYfAiKXZ
 """
 import xgboost
-import pickle
 import re
 import string
 from flask import Flask,jsonify,request
