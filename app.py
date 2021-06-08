b50906f0bb4370ea8884ce6a08f194acb7f32f39
Riza <rizakurniawan48@gmail.com>
2021-06-08 21:33:58 +0700

    update

--- app.py
+++ app.py
@@ -11,6 +11,14 @@ import string
 from flask import Flask,jsonify,request
 from sklearn.feature_extraction.text import TfidfVectorizer
 
+# load phase
+tf1 = pickle.load(open("tfidfvocab.pkl", 'rb'))
+loaded_model = pickle.load(open("xgbmodel.sav", 'rb'))
+
+#load vectorizer
+vectorizer = TfidfVectorizer(binary=True,vocabulary = tf1)
+vectorizer.fit(tf1)
+
 def text_preproc(x):
   #case folding
   x = x.lower()
@@ -28,9 +36,20 @@ def text_preproc(x):
 
 app = Flask(__name__)
 
+@app.route('/api/sentence', methods=["GET"])
+def sentece():
+    arr_text = []
+    text = request.args.get("text")
+    arr_text.append(text) 
+    clean_arr_text = list(map(text_preproc,arr_text))
+    x_sentence = vectorizer.transform(clean_arr_text)
+    y_pred = loaded_model.predict(x_sentence)
+    resp = jsonify({"text":text,"prediction":int(y_pred[0])})
+    return resp
+
 @app.route('/')
 def index():
-    return "<h1>Welcome to our server !!</h1>"
+    return "<h1>Prekocy-API !!</h1>"
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
